2c132d9e9a81d5e2f43600bcd6eecc87ff3712e8
zhengxin <43071004+guanjianhe@users.noreply.github.com>
2019-05-28 16:06:03 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,154 +5,154 @@
 
 #define IsSurrogate(c)  ((c) >= 0xD800 && (c) <= 0xDFFF)
 
-void gbk_to_utf8 ( FILE* in, FILE* out )
+void gbk_to_utf8(FILE* in, FILE* out)
 {
-    uint8_t s[4]; 
+    uint8_t s[4];
     long len, nc = 0;
     uint32_t dc;
-    
-    /* »ñÈ¡ÎÄ¼ş×Ö½ÚÊıÄ¿ */
-    fseek ( in, 0L, SEEK_END );
-    len = ftell ( in );
-    
-    /* ¶¨Î»µ½ÎÄ¼şÍ·²¿ */
-    rewind ( in );
 
-    while ( nc < len )
+    /* è·å–æ–‡ä»¶å­—èŠ‚æ•°ç›® */
+    fseek(in, 0L, SEEK_END);
+    len = ftell(in);
+
+    /* å®šä½åˆ°æ–‡ä»¶å¤´éƒ¨ */
+    rewind(in);
+
+    while (nc < len)
     {
-        fread ( s+1, sizeof ( uint8_t ), 1, in );
+        fread(s + 1, sizeof(uint8_t), 1, in);
         nc++;
         dc = s[1];
 
-		/* asciiÂë */
-        if ( dc < 0x80 )
+        /* asciiç  */
+        if (dc < 0x80)
         {
-            fwrite ( &dc, sizeof ( uint8_t ), 1, out );
+            fwrite(&dc, sizeof(uint8_t), 1, out);
         }
-        else	/* gbkÂë */ 
+        else    /* gbkç  */
         {
-            fread ( s, sizeof ( uint8_t ), 1, in );
+            fread(s, sizeof(uint8_t), 1, in);
             nc++;
-            
-            /* gbk ×ª unicode */
-            dc = * ( ( uint16_t * ) s );
-            dc = oem2uni ( dc );
 
-			/* unicode ×ª utf-8 */             
-            if ( dc < 0x800 )	/* 2×Ö½Ú */
+            /* gbk è½¬ unicode */
+            dc = * ((uint16_t*) s);
+            dc = oem2uni(dc);
+
+            /* unicode è½¬ utf-8 */
+            if (dc < 0x800)      /* 2å­—èŠ‚ */
             {
-                s[0] = ( uint8_t ) ( 0xC0 | ( dc >> 6 & 0x1F ) );
-                s[1] = ( uint8_t ) ( 0x80 | ( dc >> 0 & 0x3F ) );
-                fwrite ( s, sizeof ( uint8_t ), 2, out );
+                s[0] = (uint8_t)(0xC0 | (dc >> 6 & 0x1F));
+                s[1] = (uint8_t)(0x80 | (dc >> 0 & 0x3F));
+                fwrite(s, sizeof(uint8_t), 2, out);
             }
             else
             {
-                if ( dc < 0x10000 )	/* 3×Ö½Ú */
+                if (dc < 0x10000)    /* 3å­—èŠ‚ */
                 {
-                    s[0] = ( uint8_t ) ( 0xE0 | ( dc >> 12 & 0x0F ) );
-                    s[1] = ( uint8_t ) ( 0x80 | ( dc >> 6 & 0x3F ) );
-                    s[2] = ( uint8_t ) ( 0x80 | ( dc >> 0 & 0x3F ) );
-                    fwrite ( s, sizeof ( uint8_t ), 3, out );
+                    s[0] = (uint8_t)(0xE0 | (dc >> 12 & 0x0F));
+                    s[1] = (uint8_t)(0x80 | (dc >> 6 & 0x3F));
+                    s[2] = (uint8_t)(0x80 | (dc >> 0 & 0x3F));
+                    fwrite(s, sizeof(uint8_t), 3, out);
                 }
-                else /* 4×Ö½Ú */
+                else /* 4å­—èŠ‚ */
                 {
-                    s[0] = ( uint8_t ) ( 0xF0 | ( dc >> 18 & 0x07 ) );
-                	s[1] = ( uint8_t ) ( 0x80 | ( dc >> 12 & 0x3F ) );
-                    s[2] = ( uint8_t ) ( 0x80 | ( dc >> 6 & 0x3F ) );
-                    s[3] = ( uint8_t ) ( 0x80 | ( dc >> 0 & 0x3F ) );
-                    fwrite ( s, sizeof ( uint8_t ), 4, out );
+                    s[0] = (uint8_t)(0xF0 | (dc >> 18 & 0x07));
+                    s[1] = (uint8_t)(0x80 | (dc >> 12 & 0x3F));
+                    s[2] = (uint8_t)(0x80 | (dc >> 6 & 0x3F));
+                    s[3] = (uint8_t)(0x80 | (dc >> 0 & 0x3F));
+                    fwrite(s, sizeof(uint8_t), 4, out);
                 }
             }
         }
     }
 
-    fclose ( in );
-    fclose ( out );
+    fclose(in);
+    fclose(out);
 }
 
 
-void utf8_to_gbk ( FILE* in, FILE* out )
+void utf8_to_gbk(FILE* in, FILE* out)
 {
     uint8_t s[4];
     uint16_t rc, ct;
     uint32_t dc;
     long len, nc = 0;
 
-    /* »ñÈ¡ÎÄ¼ş×Ö½ÚÊıÄ¿ */
-    fseek ( in, 0L, SEEK_END );
-    len = ftell ( in );
+    /* è·å–æ–‡ä»¶å­—èŠ‚æ•°ç›® */
+    fseek(in, 0L, SEEK_END);
+    len = ftell(in);
 
-    /* ¶¨Î»µ½ÎÄ¼şÍ·²¿ */
-    rewind ( in );
+    /* å®šä½åˆ°æ–‡ä»¶å¤´éƒ¨ */
+    rewind(in);
 
-    while ( nc < len )
+    while (nc < len)
     {
-        /* ÏÈ¶ÁÒ»¸ö×Ö½Ú */
-        fread ( s, sizeof ( uint8_t ), 1, in );
+        /* å…ˆè¯»ä¸€ä¸ªå­—èŠ‚ */
+        fread(s, sizeof(uint8_t), 1, in);
         nc++;
 
         dc = s[0];
 
-        if ( dc >= 0x80 )
+        if (dc >= 0x80)
         {
             ct = 0;
 
-            if ( ( dc & 0xE0 ) == 0xC0 )	/* 2×Ö½Ú */
+            if ((dc & 0xE0) == 0xC0)      /* 2å­—èŠ‚ */
             {
                 dc &= 0x1F;
                 ct = 1;
             }
 
-            if ( ( dc & 0xF0 ) == 0xE0 )	/* 3×Ö½Ú */
+            if ((dc & 0xF0) == 0xE0)      /* 3å­—èŠ‚ */
             {
                 dc &= 0x0F;
                 ct = 2;
             }
 
-            if ( ( dc & 0xF8 ) == 0xF0 )	/* 4×Ö½Ú */
+            if ((dc & 0xF8) == 0xF0)      /* 4å­—èŠ‚ */
             {
                 dc &= 0x07;
                 ct = 3;
             }
 
-            if ( ct == 0 )
+            if (ct == 0)
             {
                 continue;
             }
 
-            /* ÔÙ¶Áct¸ö×Ö½Ú */
-            fread ( s, sizeof ( uint8_t ), ct, in );
+            /* å†è¯»ctä¸ªå­—èŠ‚ */
+            fread(s, sizeof(uint8_t), ct, in);
             nc += ct;
 
             rc = 0;
 
             do
             {
-                if ( ( s[rc] & 0xC0 ) != 0x80 )
+                if ((s[rc] & 0xC0) != 0x80)
                 {
                     break;
                 }
 
-                dc = dc << 6 | ( s[rc] & 0x3F );
-            } while ( ++rc < ct );
+                dc = dc << 6 | (s[rc] & 0x3F);
+            } while (++rc < ct);
 
-            /* ±àÂë²»¶Ô */
-            if ( rc != ct || dc < 0x80 || IsSurrogate ( dc ) || dc >= 0x110000 )
+            /* ç¼–ç ä¸å¯¹ */
+            if (rc != ct || dc < 0x80 || IsSurrogate(dc) || dc >= 0x110000)
             {
                 continue;
             }
 
-            dc = uni2oem ( dc );
+            dc = uni2oem(dc);
 
-            fwrite ( ( ( uint8_t* ) ( &dc ) ) + 1, sizeof ( uint8_t ), 1, out );
+            fwrite(((uint8_t*)(&dc)) + 1, sizeof(uint8_t), 1, out);
 
         }
 
-        fwrite ( &dc, sizeof ( uint8_t ), 1, out );
+        fwrite(&dc, sizeof(uint8_t), 1, out);
     }
 
-    fclose ( in );
-    fclose ( out );
+    fclose(in);
+    fclose(out);
 }
 /*
 int main ( void )
@@ -169,52 +169,51 @@ int main ( void )
         exit ( EXIT_FAILURE );
     }
 
-    printf ( "ÎÄ¼ş´ò¿ª³É¹¦\n" );
+    printf ( "æ–‡ä»¶æ‰“å¼€æˆåŠŸ\n" );
 
     gbk_to_utf8 ( in, out );
 
 }
 */
 
-int main ( void )
+int main(void)
 {
-    FILE *in, *out;
+    FILE* in, *out;
 
-    if ( ( in = fopen ( "utf8.txt", "rb" ) ) == NULL )
+    if ((in = fopen("utf8.txt", "rb")) == NULL)
     {
-        exit ( EXIT_FAILURE );
+        exit(EXIT_FAILURE);
     }
 
-    if ( ( out = fopen ( "gbk.txt", "wb" ) ) == NULL )
+    if ((out = fopen("gbk.txt", "wb")) == NULL)
     {
-        exit ( EXIT_FAILURE );
+        exit(EXIT_FAILURE);
     }
 
-    utf8_to_gbk ( in, out );
-
+    utf8_to_gbk(in, out);
 }
 
 
 #if 0
-int main ( int argc, char *argv[] )
+int main(int argc, char* argv[])
 {
 
     FILE* fp;
     int ch;
 
-    if ( argc < 2 )
+    if (argc < 2)
     {
-        exit ( EXIT_FAILURE );
+        exit(EXIT_FAILURE);
     }
 
-    if ( ( fp = fopen ( argv[1], "r" ) ) == NULL )
+    if ((fp = fopen(argv[1], "r")) == NULL)
     {
-        exit ( EXIT_FAILURE );
+        exit(EXIT_FAILURE);
     }
 
-    while ( ( ch = getc ( fp ) ) != EOF )
+    while ((ch = getc(fp)) != EOF)
     {
-        putc ( ch, stdout );
+        putc(ch, stdout);
     }
 
     return 0;
